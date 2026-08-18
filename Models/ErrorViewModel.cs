79ca173ff45ba9b9c3f22a6a0f0836b937fac9e9
siyab <siyab@SIYABONGA>
2026-08-18 22:56:29 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Professional_Profile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
